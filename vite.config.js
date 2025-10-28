9957ffdb4dac8320b341619b97f511eb55bd8164
rastumbenjamin-gif <rastum.benjamin@gmail.com>
2025-10-28 11:07:20 +0100

    Update Vite config to include base path

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss: './postcss.config.js',
-  },
+  base: '/nve-vannforing/',  // Add this line - must match your repo name
 })
